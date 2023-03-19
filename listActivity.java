513d40f06e1d4f092f5d4b66779d5103662db386
Vaishnavi Lugade <108423518+vaishnavilugade@users.noreply.github.com>
2023-03-19 12:55:06 +0530

    Update listActivity.java

--- listActivity.java
+++ listActivity.java
@@ -9,6 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+
 public class listActivity extends AppCompatActivity {
     ListView listView;
     String[] name={"Rohit","Snehal","Pankaj","Pranav","Sagar","Shivani"};
